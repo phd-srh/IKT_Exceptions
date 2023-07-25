9a514a1ea7f1ba0dc7392283051e84919008981a
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 10:29:02 +0200

    Exception mit finally-Block ohne catch-Blöcke!

---
+++ src/HeyDasGehtNichtException.java
@@ -0,0 +1,5 @@
+public class HeyDasGehtNichtException extends Exception {
+    public HeyDasGehtNichtException() {
+        super("Wir sind nicht einverstanden!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
+import java.io.FileReader;
+
 public class Main {
+
+    public static int irgendeineMethode(int divisor) throws HeyDasGehtNichtException {
+        if (divisor == 0)
+            throw new HeyDasGehtNichtException();
+        int x = 21 / divisor;
+        return 3*x;
+    }
+
     public static void main(String[] args) {
         try {
+            int y = irgendeineMethode(0);
             String eingabe = "21 Jahre";
             int i = Integer.parseInt(eingabe);
             System.out.println(i);
+            return; // obwohl return, wird der finally-Block ausgeführt
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Ja, da war was falsch mit dem Text!");
+        }
+        catch (ArithmeticException e) {
+            // was soll ich tun, wenn ein Fehler aufgetreten ist?
+            System.err.println("Habs gefangen :)");
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            System.out.println("Das gabs wohl ein Fehler!?");
         }
         finally {
-            System.out.println("Was hier passiert wird immer ausgeführt");
+            System.out.println("Was hier steht, wird immer ausgeführt");
             System.out.println("Egal ob Fehler oder kein Fehler!");
         }
         System.out.println("Puh, aber jetzt sind wir durch");
