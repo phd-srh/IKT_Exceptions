54d6d93590841872f624b4ac1ad804c217323eaf
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 09:24:01 +0200

    Exception 2: ArithmeticException

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String eingabe = "21 Jahre";
-        int i = Integer.parseInt(eingabe);
+        //String eingabe = "21 Jahre";
+        int i = 17 / 0; //Integer.parseInt(eingabe);
         System.out.println(i);
     }
 }
