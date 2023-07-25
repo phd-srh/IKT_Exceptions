b8225c7498b5017135da3768c46138b55591905f
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 09:52:54 +0200

    Exception mit finally-Block ohne catch-Blöcke!

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            //String eingabe = "21 Jahre";
-            int i = 17 / 0; //Integer.parseInt(eingabe);
+            String eingabe = "21 Jahre";
+            int i = Integer.parseInt(eingabe);
             System.out.println(i);
         }
-        catch (ArithmeticException e) {
-            // was soll ich tun, wenn ein Fehler aufgetreten ist?
-            System.err.println("Habs gefangen :)");
-            System.err.println( e.getMessage() );
-            e.printStackTrace();
+        finally {
+            System.out.println("Was hier passiert wird immer ausgeführt");
+            System.out.println("Egal ob Fehler oder kein Fehler!");
         }
         System.out.println("Puh, aber jetzt sind wir durch");
     }
