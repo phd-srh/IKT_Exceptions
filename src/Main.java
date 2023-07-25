a0b361a7a6dec091ef97172933970bd7eb83edb0
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 09:34:45 +0200

    Exception gefangen mit diversen Ausgaben!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        //String eingabe = "21 Jahre";
-        int i = 17 / 0; //Integer.parseInt(eingabe);
-        System.out.println(i);
+        try {
+            //String eingabe = "21 Jahre";
+            int i = 17 / 0; //Integer.parseInt(eingabe);
+            System.out.println(i);
+        }
+        catch (ArithmeticException e) {
+            // was soll ich tun, wenn ein Fehler aufgetreten ist?
+            System.err.println("Habs gefangen :)");
+            System.err.println( e.getMessage() );
+            e.printStackTrace();
+        }
+        System.out.println("Puh, aber jetzt sind wir durch");
     }
 }
